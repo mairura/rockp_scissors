a31c5d6f76fb1bba747073fbba340153317917bc
ted <mairurat@gmail.com>
2022-07-05 16:16:13 +0300

    resolved bets and wagers in reach

--- index.mjs
+++ index.mjs
@@ -6,14 +6,36 @@ const startingBalance = stdlib.parseCurrency(100);
 const accAlice = await stdlib.newTestAccount(startingBalance);
 const accBob = await stdlib.newTestAccount(startingBalance);
 
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
+
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const Player = (Who) => ({
+    getHand: () => {
+        const hand = Math.floor(Math.random() * 3);
+        console.log(`${Who} played ${HAND[hand]}`);
+        return hand;
+    },
+    seeOutcome: (outcome) => {
+        console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+    },
+});
+
 await Promise.all([
     ctcAlice.p.Alice({
-        //ALice interact object
+        ...Player('Alice'),
+        wager: stdlib.parseCurrency(5),
     }),
     ctcBob.p.Bob({
-        //implement Bobs interact object
-    })
-])
+        ...Player('Bob'),
+        acceptWager: (amt) => {
+            console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+        },
+    }),
+]);
--- index.rsh
+++ index.rsh
@@ -7,11 +7,35 @@ const Player = {
 
 export const main = Reach.App(() => {
     const  Alice = Participant('Alice', {
-        //Alice Interact
+        ...Player,
+        wager: UInt,
     });
     const Bob = Participant('Bob', {
-        //BOb Interact
+        ...Player,
+        acceptWager: Fun([UInt], Null)
     });
-   init();
-//Program here
+    init();
+
+    Alice.only(() => {
+        const wager = declassify(interact.wager);
+        const handAlice = declassify(interact.getHand());
+    });
+    Alice.publish(wager, handAlice).pay(wager);
+    commit();
+
+    Bob.only(() => {  
+        interact.acceptWager(wager);
+        const handBob = declassify(interact.getHand());
+    });
+    Bob.publish(handBob).pay(wager);
+
+    const outcome = (handAlice + (4 - handBob)) % 3;
+    const [forAlice, forBob] = outcome == 2 ? [ 2, 0] : outcome == 0 ? [ 0, 2] : /* tie */ [ 1, 1];
+    transfer(forAlice * wager).to(Alice);
+    transfer(forBob   * wager).to(Bob);
+    commit();
+
+    each([Alice, Bob], () => {
+        interact.seeOutcome(outcome);
+    })
 })
